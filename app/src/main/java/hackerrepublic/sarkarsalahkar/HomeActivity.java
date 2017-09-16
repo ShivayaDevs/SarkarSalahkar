a13195d2ccc79e03dc65647eaff4f67b1baf7b6c
Yash Verma <verma.yash8@gmail.com>
2017-09-16 19:51:22 +0530

    Integrated firebase

--- app/src/main/java/hackerrepublic/sarkarsalahkar/HomeActivity.java
+++ app/src/main/java/hackerrepublic/sarkarsalahkar/HomeActivity.java
@@ -1,13 +1,33 @@
 package hackerrepublic.sarkarsalahkar;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import hackerrepublic.sarkarsalahkar.models.Post;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+
+    private MyRecyclerAdapter mMyRecyclerAdapter;
+
+    private FirebaseDatabase mFirebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +36,116 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.posts_recycler_view);
+        mMyRecyclerAdapter = new MyRecyclerAdapter();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mMyRecyclerAdapter);
+
+        DatabaseReference postsRef = mFirebaseDatabase.getReference("posts");
+        postsRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.exists()) {
+                    Post post = dataSnapshot.getValue(Post.class);
+                    mMyRecyclerAdapter.addItem(post);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
 
+    private class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder> {
+
+        private ArrayList<Post> posts;
+
+        MyRecyclerAdapter() {
+            posts = new ArrayList<>();
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = getLayoutInflater().inflate(R.layout.list_item_post, parent, false);
+            return new MyViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return posts.size();
+        }
+
+        public void addItem(Post post) {
+            posts.add(post);
+            notifyDataSetChanged();
+        }
+
+        public void removeItem(Post post) {
+            posts.remove(post);
+            notifyDataSetChanged();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            private TextView authorView;
+            private TextView timeView;
+            private TextView postTitleView;
+            private TextView postPreview;
+            private TextView numStarsView;
+            private ImageView coverImageView;
+
+            MyViewHolder(View v) {
+                super(v);
+                authorView = v.findViewById(R.id.textView_author);
+                timeView = v.findViewById(R.id.textView_timestamp);
+                postTitleView = v.findViewById(R.id.textView_postTitle);
+                postPreview = v.findViewById(R.id.textView_postPreview);
+                numStarsView = v.findViewById(R.id.textView_numStars);
+                coverImageView = v.findViewById(R.id.imageView_cover);
+            }
+
+            void bind(int position) {
+                Post post = posts.get(position);
+
+                authorView.setText(post.author);
+                postTitleView.setText(post.title);
+                postPreview.setText(post.description);
+                numStarsView.setText(post.numStars + " stars");
+                timeView.setText("1 hr ago");
+                Glide.with(HomeActivity.this).load(post.imageUrl).into(coverImageView);
+            }
+        }
+    }
+
 }
+
+
+/*
+    private void uploadToFirebase(Post post) {
+        DatabaseReference postsRef = mFirebaseDatabase.getReference("posts");
+        postsRef.push().setValue(post);
+    }
+    */
--- app/src/main/java/hackerrepublic/sarkarsalahkar/models/Post.java
+++ app/src/main/java/hackerrepublic/sarkarsalahkar/models/Post.java
@@ -1,9 +1,70 @@
 package hackerrepublic.sarkarsalahkar.models;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+
 /**
  * Data model for storing user's posts.
  */
+@IgnoreExtraProperties
 public class Post {
 
+    public String author;
+    public String title;
+    public String description;
+    public String imageUrl;
+    public long creationTimestamp;
+    public int numStars = 0;
+    public int escalationLevel = 0;
 
+    public Post() {
+        // Required for call to DataSnapshot.getValue(Post.class);
+        creationTimestamp = System.currentTimeMillis();
+    }
+
+    public static class Builder {
+        Post post;
+
+        public Builder() {
+            post = new Post();
+        }
+
+        public Builder setAuthor(String author) {
+            this.post.author = author;
+            return this;
+        }
+
+        public Builder setCreationTimestamp(long timestamp) {
+            this.post.creationTimestamp = timestamp;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            post.title = title;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            post.description = description;
+            return this;
+        }
+
+        public Builder setImageUrl(String imageUrl) {
+            post.imageUrl = imageUrl;
+            return this;
+        }
+
+        public Builder setNumStars(int numStars) {
+            post.numStars = numStars;
+            return this;
+        }
+
+        public Builder setEscalationLevel(int escalationLevel) {
+            post.escalationLevel = escalationLevel;
+            return this;
+        }
+
+        public Post build() {
+            return this.post;
+        }
+    }
 }
